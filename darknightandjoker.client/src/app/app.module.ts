da569e2ab84ebe69740a5ecabaa1734aaa9890cd
JDReddington <juandamona1023@gmail.com>
2024-09-28 17:02:15 -0500

    Se agrega toastr al script para que funcione correctamente.

--- darknightandjoker.client/src/app/app.module.ts
+++ darknightandjoker.client/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { Router, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
 import { PaginaInicioSesionComponent } from '../Components/pagina-inicio-sesion/pagina-inicio-sesion.component';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PaginaInicioSesionComponent
+    
   ],
   imports: [
     BrowserModule, HttpClientModule,
